21e0e6c2bbde522edb3f8d776140aaffe166cfbb
Samuel Chiu <sivitry@gmail.com>
2023-09-21 23:13:39 +0800

    Update digiplus.user.js

--- digiplus.user.js
+++ digiplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         digiplus
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://digiplus.adi.gov.tw/*
@@ -16,7 +16,14 @@
     for(i=0; i<x.length; i++){
         if(x[i].href.indexOf("pdf")>0){
             console.log(i, x[i]);
-            x[i].setAttribute("download", x[i].title);
+            if(x[i].title.length>0){
+                x[i].setAttribute("download", x[i].title);
+            }
+            else{
+                var y = x[i].getElementsByClassName("ctn");
+                var newname = y[0].innerText;
+                x[i].setAttribute("download", newname);
+            }
         }
     }
 })();
